086bd75b4c1696ec3fce1ebb5201d4ed3369efd8
17603606331 <417560450@qq.com>
2016-09-28 15:39:20 +0800

    musicAPI  over

--- controller/music.js
+++ controller/music.js
@@ -3,15 +3,10 @@ const http = require('http');
 /**
  * 根据id获取网易云音乐
  * 测试id scott的网易云音乐id=317921676
- * ---------------------------------------
- * todo 直接调用网易接口 and 数据格式化
- * ---------------------------------------
  */
 exports.getMusicById = (req, res, next) => {
     let id = req.query.id;
-    //http://music.163.com/api/playlist/detail?id=  ,  http://120.24.162.247/api/Music?id=
     var url = `http://music.163.com/api/playlist/detail?id=${id}`;
-    var url0 = `http://120.24.162.247/api/Music?id=${id}`;
     http.get(url, (response) => {
         console.log(`Got response: ${res.statusCode}`);
         let str = "";
